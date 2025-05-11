5f285305610e5e739b04f74a960400f410e0c569
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-12 02:00:59 +0600

    feat: add save meal logic

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import sql from 'better-sqlite3';
 import DOMPurify from 'isomorphic-dompurify';
 import slug from 'slug';
@@ -10,11 +11,41 @@ export async function getMeals() {
   return db.prepare('SELECT * FROM meals').all();
 }
 
-export function getMeal(slug) {
+export async function getMeal(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
 
-export function saveMeal(meal) {
+export async function saveMeal(meal) {
   meal.slug = slug(meal.title);
   meal.instructions = DOMPurify.sanitize(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), error => {
+    if (error) {
+      throw new Error('Saving image failed!');
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+
+  db.prepare(
+    `
+    INSERT INTO meals
+      (title, summary, instructions, creator, creator_email, image, slug)
+    VALUES (
+      @title,
+      @summary,
+      @instructions,
+      @creator,
+      @creator_email,
+      @image,
+      @slug
+    )
+  `
+  ).run(meal);
 }
